3ce1a5277d72a936b5f3319791fbcbc11d9102b0
Gray Hildreth and Zach Holz <grayhildreth@gmail.com>
2016-05-23 19:31:50 -0700

    Add children to Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#bark-back").prepend("<li>Bark</li>");
+
+    $("ul#bark-back").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#bark").click(function() {
     $("ul#meow-back").prepend("<li>Meow</li>");
+
+    $("ul#meow-back").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
